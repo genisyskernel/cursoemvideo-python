607b39eb5550b4ddba9dc911b96030137c004955
genisyskernel <eduardo.doege@outlook.com>
2020-10-28 01:01:25 -0300

    Update desafio010.py

--- cursoemvideo-projeto/desafios/desafio010.py
+++ cursoemvideo-projeto/desafios/desafio010.py
@@ -1,5 +1,5 @@
-valor_real = float(input("Digite o valor na carteira: "))
+valor_real = float(input("Digite o valor na carteira? R$"))
 
 dolares = valor_real / 3.27
 
-print("O valor R$: {0} reais pode comprar USD$: {1} dolares!".format(valor_real, dolares))
+print("O valor R$: {0:.2f} reais pode comprar USD$: {1:.2f} dolares!".format(valor_real, dolares))
