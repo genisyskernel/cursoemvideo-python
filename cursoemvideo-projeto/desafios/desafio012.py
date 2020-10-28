0052a4f3a5c008fb8a17823cc461613c7e1d0a22
genisyskernel <eduardo.doege@outlook.com>
2020-10-28 01:05:48 -0300

    Update desafio012.py

--- cursoemvideo-projeto/desafios/desafio012.py
+++ cursoemvideo-projeto/desafios/desafio012.py
@@ -1,5 +1,5 @@
-preco = float(input("Digite o preco do produto: "))
+preco = float(input("Digite o preco do produto? R$"))
 
 novo_preco = preco - (preco * 0.05)
 
-print("Preco normal R$:{0}\nPreco com 5% de desconto R$: {1}".format(preco, novo_preco))
+print("Preco normal R$:{0}\nPreco com 5% de desconto R$: {1:.2f}".format(preco, novo_preco))
