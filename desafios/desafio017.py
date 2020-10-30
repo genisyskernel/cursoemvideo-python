d0a4e49b7057e17bb1770f23fc9c1959010c671d
genisyskernel <eduardo.doege@outlook.com>
2020-10-30 01:03:29 -0300

    Updates desafios

--- desafios/desafio017.py
+++ desafios/desafio017.py
@@ -4,5 +4,6 @@ cateto_oposto = float(input("Digite o comprimento do cateto oposto: "))
 cateto_adjacente = float(input("Digite o comprimento do cateto adjacente: "))
 
 hipotenusa = hypot(cateto_oposto, cateto_adjacente)
+hipotenusa_na_mao = ((cateto_oposto * cateto_oposto) + (cateto_adjacente * cateto_adjacente)) ** (1/2)
 
 print("A hipotenusa do cateto oposto {0} e do cateto adjacente {1} vale {2:.2f}.".format(cateto_oposto, cateto_adjacente, hipotenusa))
--- desafios/desafio018.py
+++ desafios/desafio018.py
@@ -1,9 +1,9 @@
-from math import sin, cos, tan
+from math import sin, cos, tan, radians
 
 angulo = float(input("Digite um angulo qualquer: "))
 
-seno = sin(angulo)
-cosseno = cos(angulo)
-tangente = tan(angulo)
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
 
 print("O angulo {0} tem o seno {1:.2f}, cosseno {2:.2f} e a tangente {3:.2f}.".format(angulo, seno, cosseno, tangente))
