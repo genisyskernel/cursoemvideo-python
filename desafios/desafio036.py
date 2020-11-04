1a7b29cc1438c94add0c559c3f8680d56aaedbe4
genisyskernel <eduardo.doege@outlook.com>
2020-11-04 03:41:47 -0300

    Update desafio036.py

--- desafios/desafio036.py
+++ desafios/desafio036.py
@@ -4,14 +4,16 @@ salario_comprador = float(input("Informe o seu salario mensal: R$ "))
 
 qtd_ano = int(input("Em quantos anos voce deseja pagar a casa? "))
 
-valor_prestacao = valor_casa / qtd_ano
-
 limite_pagamento = salario_comprador*30/100
 
-if valor_prestacao > limite_pagamento:
+valor_prestacao = valor_casa / (qtd_ano * 12)
+
+print()
+print("Valor da casa: R$ {0:.2f} reais.".format(valor_casa))
+print("Voce vai ficar {0} anos pagando a parcela.".format(qtd_ano))
+print("Valor da parcela mensal: R$ {0:.2f} reais.".format(valor_prestacao))
+
+if valor_prestacao >= limite_pagamento:
     print("Infelizmente voce nao pode FINANCIAR ESSA CASA!")
 else:
     print("Verificado que voce se QUALIFICA para a compra da CASA! Parabens!")
-    print("Valor da casa: R$ {0:.2f} reais.".format(valor_casa))
-    print("Voce vai ficar {0} anos pagando a parcela.".format(qtd_ano))
-    print("Valor da parcela mensal: R$ {0:.2f} reais.".format(valor_prestacao))
