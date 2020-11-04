797b0cd2d19e20541fea2b5da51adf1e9a032ebe
genisyskernel <eduardo.doege@outlook.com>
2020-11-04 05:08:39 -0300

    Update desafio039.py

--- desafios/desafio039.py
+++ desafios/desafio039.py
@@ -8,11 +8,15 @@ idade = ano_atual - ano_nascimento
 
 idade_alistamento = 18
 
+print("Quem nasceu no ano de {0} tem {1} anos em {2}.".format(ano_nascimento, idade, ano_atual))
+
 if(idade < idade_alistamento):
     tempo_alistamento = idade_alistamento - idade
     print("Voce ainda precisa se alistar ao servico MILITAR! Falta {0} anos.".format(tempo_alistamento))
+    print("Seu alistamento vai ser em {0}!".format(ano_atual + tempo_alistamento))
 elif(idade == idade_alistamento):
-    print("Voce esta na hora de se alistar! Ano atual {0}.".format(ano_atual))
+    print("Voce tem que se alistar IMEDIATAMENTE!")
 elif(idade > idade_alistamento):
     tempo_alistamento = idade - idade_alistamento
     print("Voce passou do tempo para se alistar! Passou {0} anos.".format(tempo_alistamento))
+    print("Seu alistamento foi em {0}!".format(ano_atual - tempo_alistamento))
