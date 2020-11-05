5247837213a5d6f4f8b5bd0b0a9188b889b029bf
genisyskernel <eduardo.doege@outlook.com>
2020-11-05 02:16:24 -0300

    Update desafio043.py

--- desafios/desafio043.py
+++ desafios/desafio043.py
@@ -2,17 +2,17 @@ altura = float(input("Informe sua altura: "))
 
 peso = float(input("Informe seu peso: "))
 
-imc = peso / (altura * 2)
+imc = peso / (altura ** 2)
 
 if(imc <= 18.5):
     situacao = "ABAIXO DO PESO"
 elif(18.5 >= imc <= 25):
     situacao = "PESO IDEAL"
-elif(25 >= imc <= 30):
+elif(imc <= 30):
     situacao = "SOBREPESO"
-elif(30 >= imc <= 40):
+elif(imc <= 40):
     situacao = "OBESIDADE"
 else:
     situacao = "OBESIDADE MORBIDA"
 
-print("Sua altura: {0:.2f} metros.\nSeu peso: {1:.2f} kilos.\nIMC: {2}!".format(altura, peso, situacao))
+print("Sua altura: {0:.2f} metros.\nSeu peso: {1:.2f} kilos.\nIMC ( {2:.2f} ): {3}!".format(altura, peso, imc, situacao))
