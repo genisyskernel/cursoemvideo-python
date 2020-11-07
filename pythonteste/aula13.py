c71c36ae73e905dc518a6917c021fb9431ba2225
genisyskernel <eduardo.doege@outlook.com>
2020-11-06 23:25:13 -0300

    Create aula13.py

---
+++ pythonteste/aula13.py
@@ -0,0 +1,29 @@
+# Contato 1 ate 6
+#for c in range(1, 7):
+#    print(c)
+#print("FIM")
+
+# Contando ao contrario
+#for c in range(6, 0, -1):
+#    print(c)
+#print("FIM")
+
+# Contando e pulando 2 em 2
+#for c in range(0, 7, 2):
+#    print(c)
+#print("FIM")
+
+#inicio = int(input("INICIO: "))
+#fim = int(input("FIM: "))
+#intervalo = int(input("INTERVALO: "))
+
+#for c in range(inicio, fim+1, intervalo):
+#    print(c)
+#print("FIM")
+
+s = 0
+for c in range(0, 3):
+    n = int(input("Digite um valor: "))
+    s += n
+print("O somatorio de todos os numeros vale: {0}!".format(s))
+print("FIM")
