9a6b425d6a7f873ed93d173f4cfc412081cef268
genisyskernel <eduardo.doege@outlook.com>
2020-11-11 03:59:38 -0300

    Update desafio049.py

--- desafios/desafio049.py
+++ desafios/desafio049.py
@@ -1,5 +1,5 @@
-numero = int(input("Digite um numero inteiro: "))
+numero = int(input("Digite um numero para ver sua tabuada: "))
 
-for c in range(0, 11):
+for c in range(1, 11):
     tabuada = numero * c
     print("{0} x {1:2} = {2:2}".format(numero, c, tabuada))
