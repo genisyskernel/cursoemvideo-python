96230d11bb20a534fdfc75cd549bdb1ce24532ab
genisyskernel <eduardo.doege@outlook.com>
2020-11-17 04:49:13 -0300

    Update desafio062.py

--- desafios/desafio062.py
+++ desafios/desafio062.py
@@ -2,17 +2,21 @@ termo = int(input("Informe o primeiro termo: "))
 
 razao = int(input("Informe a razao: "))
 
-
 tot = 1
-cont = 0
+cont = 1
 termo_final = 1
 
 while termo_final != 0:
-    if(termo_final == cont):
-        termo_final = int(input("Ate quanto mais termo voce deseja ir? "))
 
-    print("{0} -> ".format(tot), end="")
-    cont += 1
-    tot += (termo + razao) - 1
+    #print("Cont: {0}".format(cont))
+    #print("Termo final: {0}".format(termo_final))
+
+    if(cont >= termo_final ):
+        termo_final = int(input("Ate quanto mais termo voce deseja ir? "))
+        cont = 0
+    else:
+        cont += 1
+        print("{0} -> ".format(tot), end="")
+        tot += (termo + razao) - 1
 
 print("ACABOU")
