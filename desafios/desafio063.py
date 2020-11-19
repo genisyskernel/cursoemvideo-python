935c8a954842fc88367fcf0640d1ef8b82fd9ba1
genisyskernel <eduardo.doege@outlook.com>
2020-11-18 21:05:12 -0300

    Update desafio063.py

--- desafios/desafio063.py
+++ desafios/desafio063.py
@@ -1,20 +1,21 @@
 Fn = int(input("Informe um numero inteiro: "))
 
 cont = 0
+cont_1 = 0
+cont_2 = 1
 fibonacci = 0
-termo_anterior = 0
-termo_posterior = 0
 
 while cont < Fn:
 
-    termo_anterior += cont
-
-    cont += 1
-
-    termo_posterior += cont
-
-    fibonacci = termo_posterior - termo_anterior
-
     print("{0}".format(fibonacci), end=" -> ")
+
+    fibonacci = cont_1 + cont_2
+
+    cont_1 += cont_2
+
+    cont_2 = cont_1 - cont_2
+
+    # controle
+    cont += 1
 
 print("FIM")
