ce0c713b3c2ab49812aa98ab3b0ca8c79b185473
genisyskernel <eduardo.doege@outlook.com>
2020-11-20 20:11:15 -0300

    Update desafio057.py

--- desafios/desafio057.py
+++ desafios/desafio057.py
@@ -1,9 +1,12 @@
-sexo = ""
+sexo = str(input("Informe seu sexo [ M / F]: ")).strip().upper()[0]
 
 while sexo != "M" and sexo != "F":
-    sexo = str(input("Informe seu sexo [ M / F]: ")).upper()
+
+    print("Dados invalidos! ", end="")
+    sexo = str(input("Por favor informe seu sexo [ M / F ]: "))
 
 if(sexo == "M"):
     print("Seu sexo e {0}!".format("MASCULINO"))
 else:
     print("Seu sexo e {0}!".format("FEMININO"))
+print("Sexo registrado com sucesso!")
