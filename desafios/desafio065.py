59939a873a2b658c81dd82808f3b74408a8b5b4e
genisyskernel <eduardo.doege@outlook.com>
2020-11-20 04:59:26 -0300

    Update desafio065.py

--- desafios/desafio065.py
+++ desafios/desafio065.py
@@ -4,14 +4,10 @@ media = 0
 cont = 0
 maior = 0
 menor = 0
-numero_anterior = 0
-numero_atual = 0
 
 while(sair == False):
 
     numero = int(input("Informe um numero inteiro: "))
-
-    numero_atual = numero
 
     cont += 1
 
@@ -25,16 +21,13 @@ while(sair == False):
         sair = True
         media = soma / cont
 
-    if(numero_atual > numero_anterior):
-        maior = numero_atual
-    else:
-        maior = numero_anterior
-
-    if(numero_atual < numero_anterior):
-        menor = numero_atual
-    else:
-        menor = numero_anterior
-
-    numero_anterior = numero_atual
+    if(cont == 1):
+        maior = numero
+        menor = numero
+    elif(numero > maior):
+        maior = numero
+    elif(numero < menor):
+        menor = numero
 
 print("O MAIOR numero: {0} | O MENOR numero: {1}.".format(maior, menor))
+print("A media entre os {0} valores vale: {1:.2f}!".format(cont, media))
