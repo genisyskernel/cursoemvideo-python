72d81d2f2d9bce17f2a19b34f13732963b361dc8
genisyskernel <eduardo.doege@outlook.com>
2020-11-20 23:16:08 -0300

    Update desafio059.py

--- desafios/desafio059.py
+++ desafios/desafio059.py
@@ -1,31 +1,34 @@
-sair = False
+from time import sleep
+opcao = 0
 resultado = 0
 
-while sair == False:
-
-    num1 = float(input("Digite o primeiro numero: "))
-    num2 = float(input("Digite o segundo numero: "))
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+while opcao != 5:
 
     print("[ 1 ] SOMAR\n[ 2 ] MULTIPLICAR\n[ 3 ] MAIOR\n[ 4 ] NOVOS NUMEROS\n[ 5 ] SAIR\n", end="")
     opcao = int(input("Qual operacao realizar? "))
 
     if(opcao == 1):
         resultado = num1 + num2
+        print("Resultado entre {0} + {1} = {2}!".format(num1, num2, resultado))
     elif(opcao == 2):
         resultado = num1 * num2
+        print("Resultado entre {0} x {1} = {2}!".format(num1, num2, resultado))
     elif(opcao == 3):
         if(num1 > num2):
             resultado = num1
         else:
             resultado = num2
+
+        print("O maior entre {0} e {1} = {2}!".format(num1, num2, resultado))
     elif(opcao == 4):
         print("ESCOLHENDO NOVOS NUMEROS...")
+        num1 = float(input("Digite o primeiro numero: "))
+        num2 = float(input("Digite o segundo numero: "))
     elif(opcao == 5):
         print("SAINDO...")
-        sair = True
     else:
         print("Opcao invalida! Tente novamente!")
-
-    if(opcao != 4 and opcao != 5 and opcao >= 1 and opcao <= 3):
-        print("Opcao escolhida: [ {0} ]".format(opcao))
-        print("Resultado: {0}!".format(resultado))
+    print("=-=" * 10)
+    sleep(2)
