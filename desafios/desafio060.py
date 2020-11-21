3236d48879b12ad123e7b6e3ba669cc9623e1e14
genisyskernel <eduardo.doege@outlook.com>
2020-11-20 23:31:04 -0300

    Update desafio060.py

--- desafios/desafio060.py
+++ desafios/desafio060.py
@@ -1,13 +1,24 @@
-numero = float(input("Digite um numero: "))
+numero = float(input("Digite um numero para calcular seu Fatorial: "))
 
 total = 1
 
+print("Calculando o Fatorial !{} = ".format(numero), end="")
+
 while numero != 0:
-    print("{0}x".format(numero), end="")
+    print("{0}".format(numero), end="")
+    print(" x " if numero > 1 else " = ", end="")
     total *= numero
-    numero = numero - 1
+    numero -= 1
 
-if(numero == 0):
-    print(" > {0}".format(total), end="")
+#for c in range(0, int(numero)):
+#    print("{0}".format(numero), end="")
+#    print(" x " if numero > 1 else " = ", end="")
+#    total *= numero
+#    numero -= 1
 
+print("{0}".format(total), end="")
 
+# from math import factorial
+# n = int(input("Digite um numero: "))
+# f = factorial(n)
+# print("O fatorial de {0} e {1}.".format(n, f))
