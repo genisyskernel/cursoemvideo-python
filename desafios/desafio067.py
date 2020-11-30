4c672ae2dc5eb3b2032e6241379be0b767c92756
genisyskernel <eduardo.doege@outlook.com>
2020-11-30 03:10:58 -0300

    Update desafio067.py

--- desafios/desafio067.py
+++ desafios/desafio067.py
@@ -1,15 +1,12 @@
-n = cont = 0
 while True:
     n = int(input("Informe um numero para ver sua tabuada: "))
-    cont = 0
 
     if n < 0:
         break
 
     print("-" * 15)
-    while cont <= 10:
-        tabuada = n * cont
-        print(f"{n:2} x {cont:2} = {tabuada:3}")
-        cont += 1
+    for c in range(1, 11):
+        tabuada = n * c
+        print(f"{n:2} x {c:2} = {tabuada:3}")
     print("-"*15)
 print("SAINDO DO PROGRAMA DE TABUADA! Volte sempre...")
