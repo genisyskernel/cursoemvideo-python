41b274292522d3444455598447316f5a3430fa2c
Eduardo Doege <eduardo.doege@outlook.com>
2020-12-20 22:34:28 -0300

    Update desafio073.py

---
+++ desafios/desafio073.py
@@ -0,0 +1,14 @@
+tabela_brasileirao = ("Corinthians", "Palmeiras", "Santos", "Grêmio", "Cruzeiro", "Flamengo", "Vasco da Gama", "Chapecoense", "Atlêtico Mineiro", "Botafogo", "Athletico-PR", "Bahia", "São Paulo", "Fluminense", "Sport Recife", "Vitória", "Coritiba", "Avaí", "Ponte Preta", "Atlético Goianiense")
+
+# A
+print(tabela_brasileirao[:5])
+
+# B
+print(tabela_brasileirao[16:])
+
+# C
+print(sorted(tabela_brasileirao))
+
+# D
+
+print(tabela_brasileirao.index("Chapecoense"))
