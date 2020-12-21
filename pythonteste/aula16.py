7bb228e486d023627aceb15c3480e6f4c11b10e1
Eduardo Doege <eduardo.doege@outlook.com>
2020-12-20 21:31:51 -0300

    Update desafios

---
+++ pythonteste/aula16.py
@@ -0,0 +1,31 @@
+#lanche = ("Hanbúrguer", "Suco", "Pizza", "Pudim", "Batata Frita")
+# Tuplas são IMUTÁVEIS!
+# lanche[1] = "Refrigerante"
+#print(lanche)
+
+#for comida in lanche:
+#    print(f"Eu vou comer {comida}!")
+#print("Eu comi pra caramba! ;)")
+
+#for cont in range(0, len(lanche)):
+#    print(f"Eu vou comer {lanche[cont]} na posição {cont}!")
+#print("Comi pra caramba! :)")
+
+#for pos, comida in enumerate(lanche):
+#    print(f"Eu vou comer {comida} nas posição {pos}!")
+#print("Comi pra caramba! C:")
+
+#print(sorted(lanche)) # Coloca em ordem alfabetica a tupla sem modificar;
+#print(lanche)
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = a + b
+#print(c.count(9))
+#print(c.index(8)) # Pega a posicao do valor dentro da tupla;
+#print(c.index(5, 2)) # Pega a posicao do valor dentro da tupla a partir de uma posição;
+#print(c)
+
+#pessoa = ("Eduardo", 20, "Masculino", 80.0)
+#del(pessoa) # Exclui a variavel e seus valores;
+#print(pessoa) # pessoa nao existe mais;
