550d759ab46bd56fb3c28a98f83173e12db60b24
Eduardo Doege <eduardo.doege@outlook.com>
2020-12-25 02:18:53 -0300

    Update desafio072.py

--- desafios/desafio072.py
+++ desafios/desafio072.py
@@ -1,9 +1,21 @@
-numero_por_extenso = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezesete", "dezoito", "dezenove", "vinte")
+numero_por_extenso = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", 
+                      "sete", "oito", "nove", "dez", "onze", "doze", "treze", 
+                      "catorze", "quinze", "dezesseis", "dezesete", "dezoito", 
+                      "dezenove", "vinte")
 
-numero = int(input("Digite um número entre 0 e 20: "))
+sair = "N"
 
-while numero < 0 or numero > 20:
-    print("Tente novamente. ", end="")
+while sair == "N":
     numero = int(input("Digite um número entre 0 e 20: "))
+    
+    while numero < 0 or numero > 20:
+        numero = int(input("Tente novamente. Digite um número entre 0 e 20: "))
+    
+    print(f"Você digitou o número {numero_por_extenso[numero]}!")
 
-print(f"Você digitou o número {numero_por_extenso[numero]}!")
+    
+    sair = str(input("Deseja sair? [S/N]: ")).strip().upper()[0]
+    
+    if sair == "S":
+        break
+print("SAINDO... BYE!")
